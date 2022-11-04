674a49a89017e3ddf2ad4784a41236f8c2710bc7
Angélique <angelique.massolo@gmail.com>
2022-11-04 11:15:32 +0100

    idchange

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch("https://whatyearisit-backend-sand.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
-    document.querySelector("#id").textContent = data.year;
+    document.querySelector("#year").textContent = data.year;
   });
